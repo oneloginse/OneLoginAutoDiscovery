832de379cf5316d2b08e703f7e3ae5e0a386d7a4
System Administrator <root@Merritts-MacBook-Pro.local>
2013-10-07 11:11:11 -0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OneLoginAutoDiscover::Application.config.secret_key_base = 'ab2a32ffe754d215c8d694fc05a06e682b4974e155b8b785e17252c29c08f0c09739d02262386e96c0ac4d206a2638e42848d3063e4b10024ef6e8a38bae41e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OneLoginAutoDiscover::Application.config.session_store :cookie_store, key: '_OneLoginAutoDiscover_session'
